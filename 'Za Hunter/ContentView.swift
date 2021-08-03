d8d9fecb9e0bae3e4028c9890bdce17cfc6589d5
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-08-03 09:15:50 -0500

    Added pizza icon and link to store website

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -27,7 +27,9 @@ struct ContentView: View {
             showsUserLocation: true,
             userTrackingMode: $userTrackingMode,
             annotationItems: places) { place in
-            MapPin(coordinate: place.annotation.coordinate)
+            MapAnnotation(coordinate: place.annotation.coordinate) {
+                Marker(mapItem: place.mapItem)
+            }
         }
         .onAppear(perform: {
             performSearch(item: "Pizza")
@@ -50,7 +52,6 @@ struct ContentView: View {
                 
             }
         }
-        
     }
 }
 
@@ -65,4 +66,17 @@ struct Place: Identifiable {
     let annotation: MKPointAnnotation
     let mapItem: MKMapItem
 }
+
+struct Marker: View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("pizza")
+            })
+        }
+    }
+}
+
+
 
