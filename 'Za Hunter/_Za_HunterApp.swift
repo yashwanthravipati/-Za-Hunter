bf901671f71cc09307ea7cf0c6cb449d5a286058
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-08-02 22:03:25 -0500

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Yashwanth Ravipati on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
